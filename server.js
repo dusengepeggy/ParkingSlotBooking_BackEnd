01b2ccaf992d7d77f0d055eb83f217e7aea80a7d
dusengepeggy <dickinepeggy100@gmail.com>
2024-04-16 19:07:05 +0200

    adding cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express= require("express")
 const mongoose=require("mongoose")
+const cors =require("cors")
 require("dotenv").config()
 const port= process.env.PORT
 const db =process.env.MONGO_URL
 const app = express()
 const route=require("./routes")
-
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/v1',route)
