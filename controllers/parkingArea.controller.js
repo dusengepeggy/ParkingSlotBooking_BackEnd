7ce45443f35a870088a3b225da2b3fe6b3173cfa
dusengepeggy <dickinepeggy100@gmail.com>
2024-04-19 05:51:39 +0200

    editing parkingareas controller

--- controllers/parkingArea.controller.js
+++ controllers/parkingArea.controller.js
@@ -3,27 +3,27 @@ const ParkingAreas = require("../models/parkingAreas.Model")
 
 const addParking = async (req, res, next) => {
     try {
-        const alreadyExist = await ParkingAreas.findOne({ email: req.body.email })
+        const alreadyExist = await ParkingAreas.findOne({ owner: req.body.owner })
         if (alreadyExist) {
             res.status(400).json({ message: "Parking already exist" })
         }
         else {
-            
-            var totalSlots=[]
-            var lev=65
-            for(let i=1;i<=req.body.levelNumber;i++){
-                for (let j=1;j<=req.body.slotPerLevel;j++){
-                    var name=`${String.fromCharCode(lev)}${j}`
+
+            var totalSlots = []
+            var lev = 65
+            for (let i = 1; i <= req.body.levelNumber; i++) {
+                for (let j = 1; j <= req.body.slotPerLevel; j++) {
+                    var name = `${String.fromCharCode(lev)}${j}`
                     totalSlots.push({
-                        name:name,
-                        booked:false
+                        name: name,
+                        booked: false
                     })
                 }
                 lev++
-                 
+
 
             }
-            const newParking = new ParkingAreas({...req.body,slots:totalSlots})
+            const newParking = new ParkingAreas({ ...req.body, slots: totalSlots })
             const parking = await newParking.save()
             res.status(201).json({ message: "Parking added successfully", parking })
         }
@@ -35,71 +35,67 @@ const addParking = async (req, res, next) => {
 
 const getParking = async (req, res, next) => {
     try {
-        const exist = await ParkingAreas.findOne({_id:req.query.id})
+        const exist = await ParkingAreas.findOne({ _id: req.query.id })
         if (exist) {
             res.status(200).json({ message: "Parking found", parking: exist })
         }
         else {
             res.status(404).json({ message: "Parking not found" })
         }
-        
+
     } catch (error) {
-        res.status(500).json({message:"Failed to get parking area data",error:error.message})
-        
+        res.status(500).json({ message: "Failed to get parking area data", error: error.message })
+
     }
 }
 
 const updateParking = async (req, res, next) => {
 
     try {
-        const exist = await ParkingAreas.findByIdAndUpdate({_id:req.query.id},req.body,{new:true})
+        const exist = await ParkingAreas.findByIdAndUpdate({ _id: req.query.id }, req.body, { new: true })
         if (exist) {
             res.status(200).json({ message: "Parking updated successfully", parking: exist })
         }
         else {
             res.status(404).json({ message: "Parking not found" })
         }
-        
+
     } catch (error) {
-        res.status(500).json({message:"Failed to update parking area data",error:error.message})
-        
+        res.status(500).json({ message: "Failed to update parking area data", error: error.message })
+
     }
 }
 
 const deleteParking = async (req, res, next) => {
     try {
-        const exist = await ParkingAreas.findByIdAndDelete({_id:req.query.id})
+        const exist = await ParkingAreas.findByIdAndDelete({ _id: req.query.id })
         if (exist) {
             res.status(200).json({ message: "Parking deleted successfully" })
         }
         else {
             res.status(404).json({ message: "Parking not found" })
         }
-        
+
     } catch (error) {
-        res.status(500).json({message:"Failed to delete parking area data",error:error.message})
-        
+        res.status(500).json({ message: "Failed to delete parking area data", error: error.message })
+
     }
 }
 
-const getAllParkings = async (req,res,next)=>{
+const getAllParkings = async (req, res, next) => {
 
     try {
         const exist = await ParkingAreas.find({})
-        if (exist) {
-            res.status(200).json({ message: "Parkings found", parkings: exist })
-        }
-        else {
-            res.status(404).json({ message: "Parkings not found" })
-        }
-        
+
+        res.status(200).json({ message: "Parkings found", parkings: exist })
+
     } catch (error) {
-        res.status(500).json({message:"Failed to get parking area data",error:error.message})
-        
+        res.status(500).json({ message: "Failed to get parking area data", error: error.message })
+
     }
 }
 
-module.exports ={
+module.exports = {
     addParking,
     getParking,
     updateParking,
