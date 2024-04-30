cd93c23f626cc9df87773c5fcf9e14ab476a3cf2
dusengepeggy <dickinepeggy100@gmail.com>
2024-04-30 12:32:55 +0300

    model

--- controllers/user.Controller.js
+++ controllers/user.Controller.js
@@ -35,7 +35,8 @@ const updateUser = async (req, res, next) => {
         }
         else {
             const user = await UserModel.findByIdAndUpdate(req.query.id, { ...req.body })
-            res.status(200).json({ message: "User updated successfully", user: user })
+            const updatedUser =await UserModel.findOne ({_id:user._id})
+            res.status(200).json({ message: "User updated successfully", user: updatedUser })
         }
     } catch (error) {
         res.status(500).json({ message: "Error updating user", error: error.message })
